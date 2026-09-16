6316c61298d80bd42c0a54f34c262a8080b5ee84
Jens Nelke <j.nelke@brunel.net>
2026-09-16 22:00:38 +0200

    ConfigSerializer eingefügt

---
+++ ConfigSerializer.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file ConfigSerializer.cpp
+ * @brief Implementation stubs for configuration serialization helpers.
+ */
+
+#include "ConfigSerializer.h"
+
+// TODO: Implement serialization and deserialization helpers declared
+// in ConfigSerializer.h. This file intentionally provides a neutral
+// starting point for adding concrete functionality.
+
+/// @brief 
+/// @param config 
+ConfigSerializer::ConfigSerializer(const ConfigParser &config) : m_config(config)
+{
+
+}
---
+++ ConfigSerializer.h
@@ -0,0 +1,31 @@
+/**
+ * @file ConfigSerializer.h
+ * @brief Utilities to serialize and deserialize configuration data.
+ *
+ * This header declares helpers for converting configuration structures
+ * to and from a persistable format (files, streams). Implementation
+ * details live in the corresponding source file.
+ */
+
+#ifndef CONFIG_SERIALIZER_H
+#define CONFIG_SERIALIZER_H
+
+#include "ConfigParser.h"
+
+class ConfigSerializer {
+public:
+    /**
+     * @brief Serialize a ConfigParser object to a file.
+     * @param config The ConfigParser object to serialize.
+     **/
+
+     ConfigSerializer();
+     ConfigSerializer(const ConfigParser &config);
+
+private:
+    const ConfigParser& m_config;
+};
+
+
+#endif // CONFIG_SERIALIZER_H
+
